10656c7fcbd47bfcbca75ea0caa128ca6f51be78
Jungho <jungho1016@gmail.com>
2023-06-12 00:41:59 +0900

    047. iOS 스타일의 CupertinoApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_beginner/ios_style_screen.dart';
 import 'package:flutter_beginner/main_page.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MainPage(),
+      home: const IosStyleScreen(),
     );
   }
 }
